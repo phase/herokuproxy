35c5980d1fabd24f54ebc78a2a2278d950181d04
phase <ph4sesaber@gmail.com>
2016-01-15 01:54:07 -0800

    Change urls

--- herokufreeproxy/urls.py
+++ herokufreeproxy/urls.py
@@ -6,6 +6,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'herokufreeproxy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url('proxy/(?P<url>.*)', views.proxy_view),
-    url(r'^admin/', include(admin.site.urls)),
+    url('(?P<url>.*)', views.proxy_view),
 )
